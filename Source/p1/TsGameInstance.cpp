be01ae7783510059033b0379b1cc6fb7c4e55b23
eddytlxiong <eddytlxiong@tencent.com>
2025-12-26 18:22:38 +0800

    feat: init proj from mac

---
+++ Source/p1/TsGameInstance.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TsGameInstance.h"
+
+void UTsGameInstance::Init()
+{
+	Super::Init();
+}
+
+void UTsGameInstance::OnStart()
+{
+	Super::OnStart();
+
+	GameScript = MakeShared<puerts::FJsEnv>(std::make_unique<puerts::DefaultJSModuleLoader>(TEXT("JavaScript")), std::make_shared<puerts::FDefaultLogger>(), 8080);
+
+	TArray<TPair<FString, UObject*>> Arguments;
+
+	Arguments.Add(TPair<FString, UObject*>(TEXT("GameInstance"), this));
+
+	GameScript->Start(TEXT("Entry"), Arguments);
+}
+
+void UTsGameInstance::Shutdown()
+{
+	Super::Shutdown();
+
+	GameScript.Reset();
+}
