ae2256b3675d63f2b4ae989aa4f837b47eba30af
eddytlxiong <eddytlxiong@tencent.com>
2025-12-26 19:36:20 +0800

    feat: create cube in ts

--- TypeScript/Entry.ts
+++ TypeScript/Entry.ts
@@ -1,20 +1,31 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ue = require("ue");
-const puerts_1 = require("puerts");
-const world = puerts_1.argv.getByName("GameInstance").GetWorld();
+import * as ue from 'ue'
+
+import { argv } from 'puerts'
+
+const world = (argv.getByName("GameInstance") as ue.GameInstance).GetWorld() as ue.World;
+
 let actorClass = ue.StaticMeshActor.StaticClass();
+
 // 2. 设置生成时的位置和旋转
 let location = new ue.Vector(0, 0, 500); // X, Y, Z
-let rotation = new ue.Rotator(0, 0, 0); // P, Y, R
+let rotation = new ue.Rotator(0, 0, 0);  // P, Y, R
 let transform = new ue.Transform(rotation, location, new ue.Vector(1, 1, 1));
-let newActor = ue.GameplayStatics.BeginDeferredActorSpawnFromClass(world, actorClass, transform);
+
+let newActor = ue.GameplayStatics.BeginDeferredActorSpawnFromClass(
+    world, 
+    actorClass, 
+    transform, 
+) as ue.StaticMeshActor;
+
 if (newActor) {
     let meshAsset = ue.StaticMesh.Load("/Engine/BasicShapes/Cube.Cube");
+
     let smc = newActor.StaticMeshComponent;
     smc.SetStaticMesh(meshAsset);
+    
     // 5. 确保它是可见的，并且移动性正确
     smc.SetMobility(ue.EComponentMobility.Movable);
+
     // 在这里可以设置 Actor 的属性
     ue.GameplayStatics.FinishSpawningActor(newActor, transform);
     console.log("Actor 生成成功！");
